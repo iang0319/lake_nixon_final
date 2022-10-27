6a50959d052fc92261344041c3159595c59c41a5
Connor Escajeda <escajedacj@hendrix.edu>
2022-10-27 16:55:15 -0500

    Started implementing some new features to the calender

--- final_project/lib/calender_page.dart
+++ final_project/lib/calender_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:final_project/Group.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,6 +18,36 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  _CalendarPageState();
+
+  MeetingDataSource _events = MeetingDataSource(<Meeting>[]);
+  late CalendarView _currentView;
+
+  final List<CalendarView> _allowedViews = <CalendarView>[
+    CalendarView.day,
+    CalendarView.week,
+    CalendarView.workWeek,
+    CalendarView.month,
+    CalendarView.timelineDay,
+    CalendarView.timelineWeek,
+    CalendarView.timelineWorkWeek,
+    CalendarView.timelineMonth,
+  ];
+
+  /// Global key used to maintain the state, when we change the parent of the
+  /// widget
+  final GlobalKey _globalKey = GlobalKey();
+  final ScrollController _controller = ScrollController();
+  final CalendarController _calendarController = CalendarController();
+
+  @override
+  void initState() {
+    _currentView = CalendarView.week;
+    _calendarController.view = _currentView;
+    _events = MeetingDataSource(getAppointmentDetails());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,15 +77,66 @@ class _CalendarPageState extends State<CalendarPage> {
               showAgenda: true),
         ));
   }
-}
 
-List<Meeting> _getDataSource() {
-  final DateTime today = DateTime.now();
-  final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
-  final DateTime endTime = startTime.add(const Duration(hours: 2));
-  meetings.add(Meeting(
-      'Conference', startTime, endTime, const Color(0xFF0F8644), false));
-  return meetings;
+  List<Meeting> _getDataSource() {
+    final List<Color> colorCollection = <Color>[];
+    colorCollection.add(const Color(0xFF0F8644));
+    colorCollection.add(const Color(0xFF8B1FA9));
+    colorCollection.add(const Color(0xFFD20100));
+    colorCollection.add(const Color(0xFFFC571D));
+    colorCollection.add(const Color(0xFF36B37B));
+    colorCollection.add(const Color(0xFF01A1EF));
+    colorCollection.add(const Color(0xFF3D4FB5));
+    colorCollection.add(const Color(0xFFE47C73));
+    colorCollection.add(const Color(0xFF636363));
+    colorCollection.add(const Color(0xFF0A8043));
+
+    final List<Meeting> meetings = <Meeting>[];
+
+    // final DateTime today = DateTime.now();
+    // final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
+    // final DateTime endTime = startTime.add(const Duration(hours: 2));
+    // meetings.add(Meeting(
+    //     'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+
+    return meetings;
+  }
+
+  void _onViewChanged(ViewChangedDetails viewChangedDetails) {
+    if (_currentView != CalendarView.month &&
+        _calendarController.view != CalendarView.month) {
+      _currentView = _calendarController.view!;
+      return;
+    }
+
+    _currentView = _calendarController.view!;
+    SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
+      setState(() {
+        // Update the scroll view when view changes.
+      });
+    });
+  }
+
+  SfCalendar _getLakeNixonCalender(
+      [CalendarController? calendarController,
+      CalendarDataSource? calendarDataSource,
+      ViewChangedCallback? viewChangedCallback,
+      dynamic calendarTapCallback]) {
+    return SfCalendar(
+      controller: calendarController,
+      dataSource: calendarDataSource,
+      allowedViews: _allowedViews,
+      showNavigationArrow: model.isWebFullView,
+      onViewChanged: viewChangedCallback,
+      allowDragAndDrop: true,
+      showDatePickerButton: true,
+      monthViewSettings: const MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+      timeSlotViewSettings: const TimeSlotViewSettings(
+          minimumAppointmentDuration: Duration(minutes: 60)),
+      onTap: calendarTapCallback,
+    );
+  }
 }
 
 /// An object to set the appointment collection data source to calendar, which
