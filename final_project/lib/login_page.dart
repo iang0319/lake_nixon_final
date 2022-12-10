dff8dac07ccd4c797d5281caf64bef0d1ef303f0
Ian Guthrie <guthrieim@hendrix.edu>
2022-12-10 13:11:37 -0600

    User now knows if their password is incorrect

--- final_project/lib/login_page.dart
+++ final_project/lib/login_page.dart
@@ -6,6 +6,7 @@ import 'package:final_project/start_page.dart';
 import 'package:final_project/userScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'GroupPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -116,8 +117,24 @@ class _LoginScreenState extends State<LoginScreen> {
                           startPagePush();
                         } on FirebaseAuthException catch (e) {
                           if (e.code == 'user-not-found') {
+                            Fluttertoast.showToast(
+                                msg: "User not found under that email address",
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0);
                             print('No user found for that email.');
                           } else if (e.code == 'wrong-password') {
+                            Fluttertoast.showToast(
+                                msg: "Incorrect password for that email",
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 3,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0);
                             print('Wrong password provided for that user.');
                           }
                         }
